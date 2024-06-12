92b5b3f6cf68484c95b3e9937bdecd0a0820732a
TucksonDev <tucksondev@gmail.com>
2024-06-12 17:26:58 +0100

    Improve code and add tests

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Load variables from .env file
+set -o allexport
+source scripts/.env
+set +o allexport
+
+# -------------- #
+# Initial checks #
+# -------------- #
+if [ -z "$RPC_URL" ] || [ -z "$PRIVATE_KEY" ]
+then
+    echo "RPC_URL or PRIVATE_KEY is not set. Set them in the .env file"
+    exit 0
+fi
+
+cargo stylus deploy -e $RPC_URL --private-key $PRIVATE_KEY
